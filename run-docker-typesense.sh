517512a58b60230bf0062007d047d899e09d058c
Marcel Lindig <ml@nullpilot.de>
2021-08-20 08:37:33 +0200

    remove docker container when stopped

--- run-docker-typesense.sh
+++ run-docker-typesense.sh
@@ -1,5 +1,5 @@
 export TYPESENSE_API_KEY=xyz
 
-docker run -p 8108:8108 --tmpfs /data typesense/typesense:0.21.0 \
+docker run --rm -p 8108:8108 --tmpfs /data typesense/typesense:0.21.0 \
   --data-dir /data --api-key=$TYPESENSE_API_KEY
 
