c634906e4fddcfc0ff8197e15a4cb859c9fe6143
Marcel Lindig <ml@nullpilot.de>
2021-08-20 08:14:34 +0200

    use tmpfs, reset db on restart

--- run-docker-typesense.sh
+++ run-docker-typesense.sh
@@ -1,7 +1,5 @@
 export TYPESENSE_API_KEY=xyz
 
-mkdir /tmp/typesense-data
-
-docker run -p 8108:8108 -v/tmp/typesense-data:/data typesense/typesense:0.21.0 \
+docker run -p 8108:8108 --tmpfs /data typesense/typesense:0.21.0 \
   --data-dir /data --api-key=$TYPESENSE_API_KEY --name typesense-tmp
 
