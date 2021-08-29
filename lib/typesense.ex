d460d6529290c94aa81ee11de51d3b74ae04de9e
Marcel Lindig <ml@nullpilot.de>
2021-08-29 22:24:26 +0200

    improved config handling

--- lib/typesense.ex
+++ lib/typesense.ex
@@ -6,49 +6,35 @@ defmodule Typesense do
   # build dynamic client based on runtime arguments
   def client(opts \\ []) do
     middleware = [
-      {Tesla.Middleware.BaseUrl, base_url(opts)},
+      {Tesla.Middleware.BaseUrl, get_opt(opts, :base_url)},
       Tesla.Middleware.JSON,
-      {Tesla.Middleware.Headers, [{"x-typesense-api-key", api_key(opts)}]}
+      {Tesla.Middleware.Headers, [{"x-typesense-api-key", get_opt(opts, :api_key)}]}
     ]
 
     Tesla.client(middleware)
   end
 
-  defp base_url(opts) do
-    base_url = Keyword.get(opts, :base_url, false) || runtime_base_url()
+  defp get_opt(opts, key) do
+    opt = Keyword.get(opts, key) || runtime_opt(key)
 
-    unless base_url do
-      raise """
-        No base URL configured.
-      """
-    end
-
-    base_url
-  end
-
-  defp runtime_base_url do
-    case Application.get_env(:typesense, :base_url) do
-      {:system, env_key} -> System.get_env(env_key)
-      key -> key
+    case opt do
+      {:error, env_key} -> raise opt_error(key, env_key)
+      nil -> raise opt_error(key)
+      opt -> opt
     end
   end
 
-  defp api_key(opts) do
-    api_key = Keyword.get(opts, :api_key) || runtime_api_key()
-
-    unless api_key do
-      raise """
-        No API key configured.
-      """
-    end
-
-    api_key
-  end
-
-  defp runtime_api_key do
-    case Application.get_env(:typesense, :api_key) do
-      {:system, env_key} -> System.get_env(env_key)
-      key -> key
+  defp runtime_opt(opt_key) do
+    case Application.get_env(:typesense, opt_key) do
+      {:system, env_key} -> System.get_env(env_key) || {:error, env_key}
+      opt -> opt
     end
   end
+
+  defp opt_error(opt_key), do: "Required option `#{opt_key}` not configured."
+
+  defp opt_error(opt_key, env_key),
+    do: """
+      #{env_key} is not set at runtime. Set environment variable or set #{opt_key} in your config.
+    """
 end
