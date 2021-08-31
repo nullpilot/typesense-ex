3de57408181984f994d50940f9920bd8e64dcad9
Marcel Lindig <ml@nullpilot.de>
2021-08-31 21:03:33 +0200

    refactor collections tests

--- test/typesense/collections_test.exs
+++ test/typesense/collections_test.exs
@@ -12,17 +12,15 @@ defmodule Typesense.CollectionsTest do
   test "creates a new collection", %{client: client} do
     schema = build(:collection)
 
-    assert {:ok, %Tesla.Env{} = env} = Typesense.Collections.create(client, schema)
-    assert env.status == 201
-
-    assert %{
-             "name" => _,
-             "num_documents" => _,
-             "created_at" => _,
-             "fields" => _,
-             "default_sorting_field" => _,
-             "num_memory_shards" => _
-           } = env.body
+    assert {:ok,
+            %{
+              "name" => _,
+              "num_documents" => _,
+              "created_at" => _,
+              "fields" => _,
+              "default_sorting_field" => _,
+              "num_memory_shards" => _
+            }} = Typesense.Collections.create(client, schema)
   end
 
   test "retrieves an existing collection", %{client: client} do
@@ -30,53 +28,42 @@ defmodule Typesense.CollectionsTest do
 
     Typesense.Collections.create(client, schema)
 
-    assert {:ok, %Tesla.Env{} = env} =
-             Typesense.Collections.retrieve(client, "retrievecollection")
-
-    assert env.status == 200
-
-    assert %{
-             "name" => "retrievecollection",
-             "num_documents" => _,
-             "created_at" => _,
-             "fields" => _,
-             "default_sorting_field" => _,
-             "num_memory_shards" => _
-           } = env.body
+    assert {:ok,
+            %{
+              "name" => "retrievecollection",
+              "num_documents" => _,
+              "created_at" => _,
+              "fields" => _,
+              "default_sorting_field" => _,
+              "num_memory_shards" => _
+            }} = Typesense.Collections.retrieve(client, "retrievecollection")
   end
 
   test "lists all existing collections", %{client: client} do
-    schema1 = build(:collection, %{"name" => "listcollection1"})
-    schema2 = build(:collection, %{"name" => "listcollection2"})
+    schema1 = build(:collection)
+    schema2 = build(:collection)
 
     Typesense.Collections.create(client, schema1)
     Typesense.Collections.create(client, schema2)
 
-    assert {:ok, %Tesla.Env{} = env} = Typesense.Collections.list(client)
-    assert env.status == 200
-    assert is_list(env.body)
-
-    assert contains?(env.body, ["listcollection1", "listcollection2"])
+    assert {:ok, response} = Typesense.Collections.list(client)
+    assert is_list(response)
+    assert contains?(response, [schema1["name"], schema2["name"]])
   end
 
   test "deletes an existing collection", %{client: client} do
-    schema = build(:collection, %{"name" => "deletecollection"})
+    schema = build(:collection)
+    collection_name = schema["name"]
 
     # make sure collection is added successfully
-    assert {:ok, %Tesla.Env{} = env} = Typesense.Collections.create(client, schema)
-    assert env.status == 201
-
-    # confirm
-    assert {:ok, %Tesla.Env{} = env} = Typesense.Collections.list(client)
-    assert contains?(env.body, ["deletecollection"])
+    assert {:ok, _response} = Typesense.Collections.create(client, schema)
 
     # delete collection again
-    assert {:ok, %Tesla.Env{} = env} = Typesense.Collections.delete(client, "deletecollection")
-    assert env.status == 200
+    assert {:ok, _response} = Typesense.Collections.delete(client, collection_name)
 
     # confirm
-    assert {:ok, %Tesla.Env{} = env} = Typesense.Collections.list(client)
-    refute contains?(env.body, ["deletecollection"])
+    assert {:ok, response} = Typesense.Collections.list(client)
+    refute contains?(response, [collection_name])
   end
 
   defp contains?(collection_list, names_to_check) when is_list(names_to_check) do
