edacf245911d5d089b3415baa41de24ab7d4ab5b
Marcel Lindig <ml@nullpilot.de>
2021-08-31 22:19:06 +0200

    refactor documents tests

--- test/typesense/documents_test.exs
+++ test/typesense/documents_test.exs
@@ -21,7 +21,7 @@ defmodule Typesense.DocumentsTest do
         ]
       })
 
-    {:ok, %{status: 201}} = Typesense.Collections.create(client, schema)
+    {:ok, _response} = Typesense.Collections.create(client, schema)
 
     on_exit(fn -> Typesense.Collections.delete(client, collection_name) end)
 
@@ -29,64 +29,36 @@ defmodule Typesense.DocumentsTest do
   end
 
   test "create a new document", %{client: client, collection: collection} do
-    doc =
-      build(:document, %{
-        "order" => 1
-      })
+    doc = build(:document)
 
-    assert {:ok, %Tesla.Env{} = env} = Typesense.Documents.create(client, collection, doc)
-
-    assert env.status == 201
+    assert {:ok, ^doc} = Typesense.Documents.create(client, collection, doc)
   end
 
   test "upsert an existing document", %{client: client, collection: collection} do
-    doc =
-      build(:document, %{
-        "id" => "1",
-        "order" => 1
-      })
-
-    updated_doc =
-      build(:document, %{
-        "id" => "1",
-        "order" => 2
-      })
+    doc = build(:document)
+    updated_doc = %{doc | "order" => 11}
 
     Typesense.Documents.create(client, collection, doc)
-
-    assert {:ok, %Tesla.Env{} = env} = Typesense.Documents.upsert(client, collection, updated_doc)
-    assert env.status == 201
-    assert env.body == %{"id" => "1", "order" => 2}
+    assert {:ok, ^updated_doc} = Typesense.Documents.upsert(client, collection, updated_doc)
   end
 
   test "retrieve an existing document", %{client: client, collection: collection} do
-    doc =
-      build(:document, %{
-        "id" => "ret"
-      })
+    doc = build(:document)
 
     Typesense.Documents.create(client, collection, doc)
 
-    assert {:ok, %Tesla.Env{} = env} = Typesense.Documents.retrieve(client, collection, doc["id"])
-    assert env.status == 200
+    assert {:ok, ^doc} = Typesense.Documents.retrieve(client, collection, doc["id"])
   end
 
   test "update an existing document", %{client: client, collection: collection} do
-    doc =
-      build(:document, %{
-        "id" => "update",
-        "order" => 10
-      })
+    doc = build(:document)
 
     Typesense.Documents.create(client, collection, doc)
 
-    update_doc = %{doc | "order" => 11}
+    updated_doc = %{doc | "order" => 11}
 
-    assert {:ok, %Tesla.Env{} = env} =
-             Typesense.Documents.update(client, collection, doc["id"], update_doc)
-
-    assert env.status == 201
-    assert env.body == %{"id" => "update", "order" => 11}
+    assert {:ok, ^updated_doc} =
+             Typesense.Documents.update(client, collection, doc["id"], updated_doc)
   end
 
   test "delete an existing document", %{client: client, collection: collection} do
@@ -94,9 +66,7 @@ defmodule Typesense.DocumentsTest do
 
     Typesense.Documents.create(client, collection, doc)
 
-    assert {:ok, %Tesla.Env{} = env} = Typesense.Documents.delete(client, collection, doc["id"])
-
-    assert env.status == 200
+    assert {:ok, ^doc} = Typesense.Documents.delete(client, collection, doc["id"])
   end
 
   test "delete documents by query", %{client: client, collection: collection} do
@@ -107,11 +77,8 @@ defmodule Typesense.DocumentsTest do
     Typesense.Documents.create(client, collection, doc1)
     Typesense.Documents.create(client, collection, doc2)
 
-    assert {:ok, %Tesla.Env{} = env} =
+    assert {:ok, %{"num_deleted" => 2}} =
              Typesense.Documents.delete_by_query(client, collection, filter)
-
-    assert env.status == 200
-    assert env.body == %{"num_deleted" => 2}
   end
 
   test "export documents", %{client: client, collection: collection} do
@@ -121,19 +88,17 @@ defmodule Typesense.DocumentsTest do
     Typesense.Documents.create(client, collection, doc1)
     Typesense.Documents.create(client, collection, doc2)
 
-    assert {:ok, %Tesla.Env{} = env} = Typesense.Documents.export(client, collection)
-
-    assert env.status == 200
+    assert {:ok, _response} = Typesense.Documents.export(client, collection)
   end
 
   test "import documents", %{client: client, collection: collection} do
     doc1 = build(:document)
     doc2 = build(:document)
 
-    Typesense.Documents.import(client, collection, [doc1, doc2], action: "create", batch_size: 5)
-
-    assert {:ok, %Tesla.Env{} = env} = Typesense.Documents.export(client, collection)
-
-    assert env.status == 200
+    assert {:ok, _response} =
+             Typesense.Documents.import(client, collection, [doc1, doc2],
+               action: "create",
+               batch_size: 5
+             )
   end
 end
