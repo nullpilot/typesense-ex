8c6e29d46666669a0545b0c954a96772765b8ca8
Marcel Lindig <ml@nullpilot.de>
2021-09-05 23:08:50 +0200

    add application scaffold

--- mix.exs
+++ mix.exs
@@ -20,7 +20,8 @@ defmodule Typesense.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Typesense.Application, []}
     ]
   end
 
