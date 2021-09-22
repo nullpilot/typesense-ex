1a6b55b20ebcf5eb4abcdc901d2a396c653fbcd1
Marcel Lindig <ml@nullpilot.de>
2021-09-22 12:45:26 +0200

    test: test healtchcheck against application

--- test/typesense/healthcheck_test.exs
+++ test/typesense/healthcheck_test.exs
@@ -1,37 +1,34 @@
 defmodule Typesense.HealthcheckTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case
 
   alias Typesense.Healthcheck
 
-  @node "http://localhost:8108/"
   @interval 50
 
-  setup_all context do
-    start_supervised!(Healthcheck)
-    context
-  end
-
   test "confirm viability of new node" do
-    assert true == Healthcheck.is_viable(@node, @interval)
+    assert true == Healthcheck.is_viable("http://new-node:8108/", @interval)
   end
 
   test "confirm viability of a healthy node" do
-    Healthcheck.pass_check(@node)
+    node = "http://healthy-node:8108/"
+    Healthcheck.pass_check(node)
 
-    assert true == Healthcheck.is_viable(@node, @interval)
+    assert true == Healthcheck.is_viable(node, @interval)
   end
 
   test "confirm viability of a non-healthy node after the healthcheck interval" do
-    Healthcheck.fail_check(@node)
+    node = "http://unhealthy-node:8108/"
+    Healthcheck.fail_check(node)
 
     :timer.sleep(@interval + 100)
 
-    assert true == Healthcheck.is_viable(@node, @interval)
+    assert true == Healthcheck.is_viable(node, @interval)
   end
 
   test "refute viability of a non-healthy node" do
-    Healthcheck.fail_check(@node)
+    node = "http://unhealthy-node:8208/"
+    Healthcheck.fail_check(node)
 
-    assert false == Healthcheck.is_viable(@node, @interval)
+    assert false == Healthcheck.is_viable(node, @interval)
   end
 end
